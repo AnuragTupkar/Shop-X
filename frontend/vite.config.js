ef2a23bc806a6ed02facfd384eb62d5ba58c8d5a
Anurag <anuragtupkar168@gmail.com>
2025-01-19 12:28:28 +0530

    deploy dot env addded

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,14 +1,17 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import dotenv from 'dotenv';
+
+// Load environment variables from .env file
+dotenv.config();
 
 export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: process.env.VITE_API_BASE_URL,  // Ensure 'http://' is included
+        target: process.env.VITE_API_BASE_URL, // Use process.env for Vite config
         changeOrigin: true,
         secure: false,
-
       },
     },
   },
